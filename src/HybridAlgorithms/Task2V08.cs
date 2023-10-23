1cff61cee4b71deb7c3af8736ac2f063ce3dbd1f
SofiaReznychenko <RSV0990787378@gmail.com>
2023-10-23 15:20:08 +0300

    updated task 2

--- src/HybridAlgorithms/Task2V08.cs
+++ src/HybridAlgorithms/Task2V08.cs
@@ -1,9 +1,37 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Xml;
+
 namespace HybridAlgorithms;
 
 public class Task2V08
 {
     public List<int> Calculate(int digitCount)
     {
+        var List = new List<int>();
+        for (int i = 0; i < 10000; i++)
+        {
+            if (IsKapricalNumber(i))
+            {
+                List.Add(i);
+            }
+        }
         throw new NotImplementedException();
+    }
+    bool IsKapricalNumber(int number)
+    {
+        string strPower = $"{number * number}";
+
+        for (int i = 1; i < strPower.Length; i++)
+        {
+            int part1 =
+                int.Parse(strPower.Substring(0, i));
+            int part2 =
+                int.Parse(strPower.Substring(i));
+            if (part1 + part2 == number)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
